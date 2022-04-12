6d3b0eba8a90c8f1fd7d69f3601f3f495089938c
Thiago Sanches <tsigwt@gmail.com>
2022-04-12 18:02:23 -0300

    Update to add two node functions that receive one object and an array

--- main.js
+++ main.js
@@ -1,22 +1,67 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const fs = require('fs');
+const uuid = require('uuid');
+
 const { exec } = require('child_process');
 
 const app = express()
+app.use(bodyParser.json())
 
 app.post('/sendMessage', function (req, res) {
-  
-  exec('java -jar /home/whatsapp/sikulixide-2.0.5.jar -r /home/whatsapp/app/sikulixide.template.py', (error, stdout, stderr) =>{
-    if(error){
+  const { name, body } = req.body;
+
+  const fileTemplate = fs.readFileSync('/home/whatsapp/app/sikulixide.template.py', 'utf8')
+  const fileContent = fileTemplate.replace("@NAME", name).replace("@BODY", body);
+  const fileName = `${uuid.v4()}.sikulix.py`;
+  fs.writeFileSync(fileName, fileContent, { encoding: "utf8" });
+
+  console.log(fileName);
+  console.log(fileContent);
+
+  exec(`java -jar /home/whatsapp/sikulixide-2.0.5.jar -r ${fileName}`, (error, stdout, stderr) => {
+    if (error) {
       console.log(error)
     }
 
-    if(stderr){
+    if (stderr) {
       console.log(stderr)
     }
-    
+
     console.log(stdout)
   })
-  
+  res.send('OK');
+})
+
+app.post('/sendMessages', function (req, res) {
+
+  const fileTemplate = fs.readFileSync('/home/whatsapp/app/sikulixide.template.py', 'utf8')
+  const fileName = `${uuid.v4()}.sikulix.py`;
+
+  let fileContent = "";
+  const data = req.body;
+  for (var i = 0; i < data.length; i++) {
+    fileContent += fileTemplate.replace("@NAME", data[i].name).replace("@BODY", data[i].body) + "\n";
+
+  }
+
+  fs.writeFileSync(fileName, fileContent, { encoding: "utf8" });
+
+  console.log(fileName);
+  console.log(fileContent);
+
+  exec(`java -jar /home/whatsapp/sikulixide-2.0.5.jar -r /home/whatsapp/app/${fileName}`, (error, stdout, stderr) => {
+    if (error) {
+      console.log(error)
+    }
+
+    if (stderr) {
+      console.log(stderr)
+    }
+
+    console.log(stdout)
+  })
+  res.send('OK');
 })
 
 app.listen(3000)
--- resources/sikulixide.template.py
+++ resources/sikulixide.template.py
@@ -1,7 +1,7 @@
 click(Location(20,34));
 click(Location(96, 270));
-type('Test');
+type('@NAME');
 click(Location(128, 400));
 click(Location(558, 702));
-type('Hello World');
+type('@BODY');
 click(Location(986, 704));
