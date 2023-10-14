6fb05669ba48b63121476abfcaa918acfb3e49fe
Thiago Sanches <tsigwt@gmail.com>
2023-10-13 21:46:28 -0300

    Update to fix and install playwright prior to the first execution

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -10,6 +10,7 @@ PATH=$PATH:"/home/$APP_USER/.nvm/versions/node/v18.18.2/bin"
 
 # I know, it's not a good practice to run two entrypoint process on the same container,
 # but my node application needs to interact with sikulixide that lives on the same container and has a X session.
+cd ~/app && npx playwright install
 DISPLAY=:1 node ~/app/main.js &
 vncserver -geometry 1024x768 :1
 
