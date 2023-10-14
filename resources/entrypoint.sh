498230528842a765546310398efe81b87f5549d4
Thiago Sanches <tsigwt@gmail.com>
2023-10-13 21:42:26 -0300

    Update to fix the node path

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -6,7 +6,7 @@ rm -rf /tmp/.X0-lock
 rm -rf /tmp/.X11-unix/X1
 rm -rf /tmp/.X1-lock
 echo "Running as user: $APP_USER"
-PATH=$PATH:"/home/$APP_USER/.nvm/versions/node/v14.21.3/bin"
+PATH=$PATH:"/home/$APP_USER/.nvm/versions/node/v18.18.2/bin"
 
 # I know, it's not a good practice to run two entrypoint process on the same container,
 # but my node application needs to interact with sikulixide that lives on the same container and has a X session.
