687a1a96fad4addb280b54329c86dccebc89fa4f
Thiago Sanches <tsigwt@gmail.com>
2023-11-04 08:33:37 -0300

    Update the way it replies back the people on a chat.

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ async function autoReplyUnreadMessages() {
 
         // The first thing is the contact name,
         // I know that's ugly, but it's a pain inspect everything.
-        const contactName = mainDiv.split('\n')[0].trim();
+        const groupName = mainDiv.split('\n')[0].trim();
         const elements = await page.evaluate(async () => {
             // Eliminate any div that displays the small URL preview,
             // as it can sometimes lead to comprehension issues for GPT.
@@ -127,105 +127,114 @@ async function autoReplyUnreadMessages() {
         });
 
         data.push({
-            contactName: contactName,
+            groupName: groupName,
             messages: elements
         });
 
         logger.info('[chat received] %o', data);
+        if (!chatMessagesHistory[groupName]) { chatMessagesHistory[groupName] = []; }
 
-        if (!chatMessagesHistory[contactName]) {
-            chatMessagesHistory[contactName] = {};
-            chatMessagesHistory[contactName].messages = [];
-            chatMessagesHistory[contactName].messages.push({ role: "system", content: config.openaiBotChatPrompt });
-        }
-
-        // Prepare just the messages that have mention to the bot user, 
-        // and get always the latest message which is the message itself that we want it.
+        // Prepare just the messages that have mention to the bot user.
         // TODO: make it better the properties names below, it's confusing.
         for (const message of data[0].messages) {
             for (const bla of message.message) {
-                const name = message.name
+                const person = message.name
                 const chat = bla[bla.length - 1];
                 if (chat.indexOf('@' + config.openaiBotName) >= 0) {
-                    chatMessagesHistory[contactName].messages.push({
-                        role: "user", content: `${name} ${config.openaiBotSaidKeyword}: "${chat}"`
+                    let filteredPerson = chatMessagesHistory[groupName].find(a => a.name === person);
+                    if (!filteredPerson) {
+                        logger.info("first message from person: %o", person);
+                        filteredPerson = {
+                            name: person,
+                            messages: [
+                                { role: "system", content: config.openaiBotChatPrompt }
+                            ]
+                        }
+                        chatMessagesHistory[groupName].push(filteredPerson);
+                    }
+                    filteredPerson.name = person;
+                    filteredPerson.messages.push({
+                        role: "user", content: `${person} ${config.openaiBotSaidKeyword}: "${chat}"`
                     })
                 }
             }
         }
 
-        // clear a little bit the content prior to forward to ChatGPT.
-        logger.info("[chatMessagesHistory] %o", chatMessagesHistory[contactName].messages);
-        logger.info("[openaiBotName] %o", config.openaiBotName);
-        logger.info("[openaiBotTurnedOn] %o", config.openaiBotTurnedOn);
+        for (const personMessages of chatMessagesHistory[groupName]) {
+            const currentPersonMessages = personMessages.messages;
+            // clear a little bit the content prior to forward to ChatGPT.
+            logger.info("[chatMessagesHistory] %o", currentPersonMessages);
+            logger.info("[openaiBotName] %o", config.openaiBotName);
+            logger.info("[openaiBotTurnedOn] %o", config.openaiBotTurnedOn);
 
-        // only answer if you have been mentioned and the bot is turned on.
-        if (chatMessagesHistory[contactName].messages.length > 1 &&
-            config.openaiBotTurnedOn) {
+            // only answer if you have been mentioned and the bot is turned on.
+            if (currentPersonMessages.length > 1 &&
+                config.openaiBotTurnedOn) {
 
-            logger.info("🤖 Bot mentioned and turned on!");
-            const configuration = new Configuration({ apiKey: config.openaiBotKey });
-            const openai = new OpenAIApi(configuration);
+                logger.info("🤖 Bot mentioned and turned on!");
+                const configuration = new Configuration({ apiKey: config.openaiBotKey });
+                const openai = new OpenAIApi(configuration);
 
-            const possibleAnswers = await openai.createChatCompletion({
-                model: config.openaiBotModel,
-                max_tokens: config.openaiBotMaxTokens,
-                temperature: config.openaiBotTemperature,
-                messages: chatMessagesHistory[contactName].messages
-            });
+                const possibleAnswers = await openai.createChatCompletion({
+                    model: config.openaiBotModel,
+                    max_tokens: config.openaiBotMaxTokens,
+                    temperature: config.openaiBotTemperature,
+                    messages: currentPersonMessages
+                });
 
-            let answer = possibleAnswers.data.choices[0].message.content.replaceAll('\\n', '').trim();
-            chatMessagesHistory[contactName].messages.push({ role: "assistant", content: answer });
-            logger.info('[chatGPT response] %o', answer);
+                let answer = possibleAnswers.data.choices[0].message.content.replaceAll('\\n', '').trim();
+                currentPersonMessages.push({ role: "assistant", content: answer });
+                logger.info('[chatGPT response] %o', answer);
 
-            // if for some reason the message contains that magic command 'photo:true' (openaiBotCommandPhoto),
-            // it will try to generate an image with DALLE, in order to send it, but only if it's still on the limit.
-            if (answer.trim().toLowerCase().includes(config.openaiBotCommandPhoto)) {
-                if (totalPhotosTakenByDay <= config.openaiBotTotalPhotosLimit) {
-                    const activities = config.openaiBotActivities;
-                    const photoFinalPrompt = config.openaiBotDallePrompt.replaceAll("##TEXT##",
-                        activities[Math.floor(Math.random() * activities.length)]);
+                // if for some reason the message contains that magic command 'photo:true' (openaiBotCommandPhoto),
+                // it will try to generate an image with DALLE, in order to send it, but only if it's still on the limit.
+                if (answer.trim().toLowerCase().includes(config.openaiBotCommandPhoto)) {
+                    if (totalPhotosTakenByDay <= config.openaiBotTotalPhotosLimit) {
+                        const activities = config.openaiBotActivities;
+                        const photoFinalPrompt = config.openaiBotDallePrompt.replaceAll("##TEXT##",
+                            activities[Math.floor(Math.random() * activities.length)]);
 
-                    const photoResponse = await openai.createImage({
-                        prompt: photoFinalPrompt,
-                        n: 1,
-                        size: "256x256",
-                    });
+                        const photoResponse = await openai.createImage({
+                            prompt: photoFinalPrompt,
+                            n: 1,
+                            size: "256x256",
+                        });
 
-                    const photoUrl = photoResponse.data.data[0].url;
-                    const page2 = await browser.newPage();
-                    await page2.goto(photoUrl);
-                    await page2.keyboard.press("Control+C");
-                    await page.bringToFront();
-                    await page.keyboard.press("Control+V");
+                        const photoUrl = photoResponse.data.data[0].url;
+                        const page2 = await browser.newPage();
+                        await page2.goto(photoUrl);
+                        await page2.keyboard.press("Control+C");
+                        await page.bringToFront();
+                        await page.keyboard.press("Control+V");
 
+                        await page.waitForTimeout(1000);
+                        await page.locator('[aria-label="Send"]>>nth=0').click();
+                        await page.waitForTimeout(5000);
+                        await page2.close();
+                        totalPhotosTakenByDay++;
+                    }
+                    else {
+                        answer = config.openaiBotCommandPhotoFailed;
+                    }
+                }
+
+                // TODO: make it better to organize it.
+                if (answer.trim().toLowerCase().includes(config.openaiBotCommandScrape)) {
+                    answer = await scrape.run(logger, answer);
+                }
+
+                // Sometimes GPT respond back with only the command defined as a 'placeholder' for photos like: 'photo:true',
+                // and I don't want it to answer with that value! 
+                // I noticed that even with a normal phrase, it puts back the 'photo:true', so we need to sanitize it.            
+                if (answer.trim().toLowerCase() !== config.openaiBotCommandPhoto) {
+                    const pattern = new RegExp(config.openaiBotCommandPhotoRegex, "gmi"); // The "i" flag makes it case-insensitive
+                    const filteredAnswer = answer.replace(pattern, '');
+
+                    await page.type('div[title="Type a message"]', filteredAnswer);
                     await page.waitForTimeout(1000);
                     await page.locator('[aria-label="Send"]>>nth=0').click();
-                    await page.waitForTimeout(5000);
-                    await page2.close();
-                    totalPhotosTakenByDay++;
+                    await page.waitForTimeout(1000);
                 }
-                else {
-                    answer = config.openaiBotCommandPhotoFailed;
-                }
-            }
-
-            // TODO: make it better to organize it.
-            if (answer.trim().toLowerCase().includes(config.openaiBotCommandScrape)) {
-                answer = await scrape.run(logger, answer);
-            }
-
-            // Sometimes GPT respond back with only the command defined as a 'placeholder' for photos like: 'photo:true',
-            // and I don't want it to answer with that value! 
-            // I noticed that even with a normal phrase, it puts back the 'photo:true', so we need to sanitize it.            
-            if (answer.trim().toLowerCase() !== config.openaiBotCommandPhoto) {
-                const pattern = new RegExp(config.openaiBotCommandPhotoRegex, "gmi"); // The "i" flag makes it case-insensitive
-                const filteredAnswer = answer.replace(pattern, '');
-
-                await page.type('div[title="Type a message"]', filteredAnswer);
-                await page.waitForTimeout(1000);
-                await page.locator('[aria-label="Send"]>>nth=0').click();
-                await page.waitForTimeout(1000);
             }
         }
     } catch (e) {
